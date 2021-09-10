155674a418a3d13face5c69eb6b65031450a0909
peterbroadhead-hothd <peterbroadhead@houseonthehill.com>
2021-09-10 11:18:09 +0100

    Push test

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 	<html <?php language_attributes(); ?>>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<title>*<?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link href="<?php echo get_stylesheet_directory_uri(); ?>/fonts/fontawesome-pro-5.3.1-web/css/all.css" rel="stylesheet">
 	<link rel="shortcut icon" href="<?php echo site_url(); ?>/wp-content/uploads/2014/07/hoth.jpg" />
