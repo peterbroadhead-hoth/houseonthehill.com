2d1222975cb5b11ed80372e5cd37dcd9046eb644
peterbroadhead-hothd <peterbroadhead@houseonthehill.com>
2021-09-14 11:35:52 +0100

    Mailchimp Signup

--- inc/mailchimp.php
+++
@@ -1,4 +0,0 @@
-<!-- Begin MailChimp Signup Form -->
-<h2 style="color:#ccc;font-weight:100;margin-top:0;font-size:20px">Stay in the Loop</h2>
-<p style="color:#ccc;font-weight:100; font-size:13px;letter-spacing:1px">Keep up to date with all things HotH by signing up to our mailing list</p>
-<?php echo do_shortcode("[mc4wp_form]"); ?>
