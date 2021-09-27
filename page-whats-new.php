b5accc063ca39ebc3ee232bee79f8fbbb110a085
Luke Broadhead <lukebroadhead@houseonthehill.com>
2021-09-27 15:46:59 +0100

    whats new

--- page-whats-new.php
+++ page-whats-new.php
@@ -56,20 +56,32 @@
       <p>The Mail API allows service desk agents to set ticket properties in the body of an email when creating a new ticket or responding to an existing ticket.</p>
     </li>
     <li>
-      <h2></h2>
-      <p></p>
+      <h2>Activities/Tasks</h2>
+      <p>Choose your Activity Form on Activity category setup. The benefit is that you can create an Activity Form and use the form for various Types of Activity.</p>
+      <img height="25" width="55" src="<?php echo get_stylesheet_directory_uri(); ?>/images/screenshots/activityjobcosting.jpg" alt="activity / job costing">
+      <p><strong>Pick the columns shown when looking at Activities on a ticket.</strong> So many of you have asked for this! 
+As long as you are using the browser and have HouseForms, you can create a Form to determine the columns to show when listing Activities on a ticket.
+The Activity Summary Form must be called CallActivity_Summary_<folder:no></p>
     </li>
     <li>
-      <h2></h2>
-      <p></p>
+      <h2>Email</h2>
+      <p>E-Mail Settings Tidy up and consolidation
+This is really for new systems, if your email is already set up already and working then don’t mess with it. But if you go to the Settings Cog and Email Settings you will see it is easy to navigate to the outbound and inbound mail Settings.<br><br>And you can see from the image above that we have added a filter box to help you find the mail template you are looking for.
+<br><br> If you have a lot of User Defined templates, you might find it useful to know if the template is used by any Categories. If it isn’t linked to a Category, you can delete a User Defined Email Template, should you have a need to tidy up.</p>
     </li>
     <li>
-      <h2></h2>
-      <p></p>
+      <h2>Summary Lists</h2>
+      <p>Have you got lots of Types or Sub-Types and can’t find the one you are looking for? Then the new Filter by Name box will help. <br><br>We have no-end of support requests wanting to know how to Save a View. Now it’s accessible from a button at the top of the Ticket Summary!
+      <br><br>If you keep create loads of Notes on tickets, then the new filtering check boxes will help to reduce the volume.</p>
     </li>
     <li>
-      <h2></h2>
-      <p></p>
+      <h2>Link rather than Attach Files</h2>
+      <p>We have introduced the ability to add links to files as an alternative to physical attachments</p>
+    </li>
+    <li>
+      <h2>Bespoke Login Roles</h2>
+      <p>This new feature is useful if you have a lot of login records to manage.<br>
+Create a new Role Login by checking the Role box. <br><br>Use the Role on new and existing Logins to replicate the settings. If you amend the Role, then all the logins with that Role are also updated.</p>
     </li>
   </ul>
 </div>
