13589af7a034a5362de892a4c87535d4d4e9e5e1
Luke Broadhead <lukebroadhead@houseonthehill.com>
2022-01-11 14:11:13 +0000

    test

---
+++ test.php
@@ -0,0 +1,19 @@
+<table class="prodMenu">
+<tr><td>
+<div class="dropdown2">
+<button class="dropbtn2">SPCGuidance</button>
+    <div id="m1" class="dropdown2-content">
+        <a href="#">[PR]:4-hr Calibrated Tornado Probability</a>
+        <a href="#">[PR]:4-hr Calibrated Hail Probability</a>
+    </div>
+</div>
+</td>
+<td>
+<div class="dropdown2">
+<button class="dropbtn2">Reflectivity</button>
+    <div id="m2" class="dropdown2-content">
+        <a href="#">[SP]:3-hr 1-km Reflectivity >=40 dBZ</a>
+        <a href="#">[NPRS]:3-hr 1-km Reflectivity >=40 dBZ</a>
+    </div>
+</div>
+</td>
