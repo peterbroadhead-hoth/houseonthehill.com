c332014d56c44be68181498066aa12192226b679
peterbroadhead-hothd <peterbroadhead@houseonthehill.com>
2022-01-14 15:30:58 +0000

    Updated img url

--- page-home.php
+++ page-home.php
@@ -54,7 +54,7 @@
       </div>
       <li class="foi">
         <h3>FOI, Case & Complaints <br>Management</h3>
-        <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/foi-Header.jpg" alt="foi Icon"></div>
+        <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/foi-header.jpg" alt="foi Icon"></div>
         <p>A popular fully paperless solution to aid your team with new requests logged by the public via the portal or by email. These cases can then be categorised into types and workflows such as FOI, Data Protection, Subject Access Requests, Complaints, Compliments, Information Governance and RoPA. </p>
         <div class="btn">
           <a class="sign-up" target="_blank" href="https://house-on-the-hill.com/mothership/register?A=QC&QC=QC000001">Try for free</a>
@@ -64,7 +64,7 @@
 
       <li class="itsm">
         <h3>IT Service <br>Management</h3>
-        <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/itsm-Header.jpg" alt="itsm Icon"></div>
+        <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/itsm-header.jpg" alt="itsm Icon"></div>
         <p>Deliver a modern ITIL driven service desk with a deep set of tools, automated workflows and a self-service portal. Where your users can raise tickets, access common service requests and search the knowledge base for FAQs.  </p>
         <div class="btn">
           <a class="sign-up" target="_blank" href="https://house-on-the-hill.com/mothership/register?A=QC&QC=QC000006">Try for free</a>
@@ -74,7 +74,7 @@
 
       <li class="csm">
         <h3>Customer Service <br>Management</h3>
-        <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/CSM-Header.jpg" alt="Customer Service Management Icon"></div>
+        <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/csm-header.jpg" alt="Customer Service Management Icon"></div>
         <p>Helps your sales team provide high quality customer support. Keep customers and staff happy by capturing key information through the branded portal, chat, email, feedback surveys and use data driven decisions from reports and KPIs to continually improve your service.</p>
         <div class="btn">
           <a class="sign-up" target="_blank" href="https://house-on-the-hill.com/mothership/register?A=QC&QC=QC000004">Try for free</a>
@@ -84,7 +84,7 @@
 
       <li class="cafm">
         <h3>Computer Aided Facilities <br>Management</h3>
-        <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/Cafm-Header.jpg" alt="CAFM Icon"></div>
+        <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/cafm-header.jpg" alt="CAFM Icon"></div>
         <p>Will allow you to manage your whole property portfolio, jobs and keep your engineers on the road. Your tool kit will include an engineer mobile app, reactive job logging, property maps, planned preventative maintenance, Job Sheets, Quotes, Invoices and more! </p>
         <div class="btn">
           <a class="sign-up" target="_blank" href="https://house-on-the-hill.com/mothership/register?A=QC&QC=QC000003">Try for free</a>
@@ -100,7 +100,7 @@
         <h2 style="color:white">Popular features</h2>
       </div>
       <li>
-        <div class="img"><img src="https://hoth.local/wp-content/themes/houseonthehill.com/images/screenshots/hoth-portal.webp" alt="custom hoth portal" /></div>
+        <div class="img"><img src="/wp-content/themes/houseonthehill.com/images/screenshots/hoth-portal.webp" alt="custom hoth portal" /></div>
         <div class="text">
           <h4>Branded Self-Service</h4>
           <p>And End User experience they will love. Empower your users with their own self-service portal where they can manage their tickets, access knowledge and access common requests.</p>
