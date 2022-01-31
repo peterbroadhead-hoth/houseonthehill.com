665b874b1b3e1d246848c6626440d55c2371a8a8
Luke Broadhead <lukebroadhead@houseonthehill.com>
2022-01-31 12:36:23 +0000

    home

--- page-home.php
+++ page-home.php
@@ -183,51 +183,50 @@
       <div class="text">
         <h2>Get the keys to your new home</h2>
         <p>Start your free 30 day trial. No credit card required.</p>
+        <div class="form">
+          <form action="https://house-on-the-hill.com/mothership/supisacs.dll" method="POST" id="theForm" name="theForm" onsubmit="return Form_Validator(this);" enctype="multipart/form-data">
+            <input type="HIDDEN" id="ID" name="ID" value="52d8bfffda342ee">
+            <input type="HIDDEN" id="entrytoggle" name="entrytoggle" value="">
+            <input type="HIDDEN" id="selchange" name="selchange" value="">
+            <input type="HIDDEN" id="selparam1" name="selparam1" value="">
+            <input type="HIDDEN" id="AjaxUpdate" name="AjaxUpdate" value="">
+            <input type="HIDDEN" id="A" name="A" value="AnonCall">
+            <input type="HIDDEN" id="F1" name="F1" value="">
+            <input type="HIDDEN" id="F2158" name="F2158" value="0">
+            <input type="HIDDEN" id="NextStep" name="NextStep" value="">
+            <input type="HIDDEN" id="F2126" name="F2126" value="341">
+            <input type="HIDDEN" id="F2127_ctrl" name="F2127_ctrl" value="">
+            <input type="HIDDEN" id="F2128_ctrl" name="F2128_ctrl" value="">
+            <input type="HIDDEN" id="F2150_ctrl" name="F2150_ctrl" value="">
+            <input type="HIDDEN" id="F2129_ctrl" name="F2129_ctrl" value="">
+            <input type="HIDDEN" id="F2130_ctrl" name="F2130_ctrl" value="">
+            <input type="HIDDEN" id="F2148_ctrl" name="F2148_ctrl" value="">
+            <input type="HIDDEN" id="F2149_ctrl" name="F2149_ctrl" value="">
+            <input type="HIDDEN" id="ActResolved_ctrl" name="ActResolved_ctrl" value="">
+            <input type="HIDDEN" id="ActRespondedTo_ctrl" name="ActRespondedTo_ctrl" value="">
+            <input type="HIDDEN" id="F5006_ctrl" name="F5006_ctrl" value="">
+            <input type="HIDDEN" id="ContactId" name="ContactId" value="0">
+            <input type="HIDDEN" id="F" name="F" value="0">
+            <input type="HIDDEN" id="iViewHistory" name="iViewHistory" value="0">
+            <input type="HIDDEN" id="CCSM" name="CCSM" value="0">
+            <input type="HIDDEN" id="InitMedium" name="InitMedium" value="8192">
+            <input type="HIDDEN" id="SummViewFold" name="SummViewFold" value="0">
+            <input type="HIDDEN" id="CUOCHF" name="CUOCHF" value="1">
+            <input type="HIDDEN" id="savebtn" name="savebtn" value="">
+            <input type="HIDDEN" id="savebtn1" name="savebtn1" value="">
+            <input type="HIDDEN" id="CFID" name="CFID" value="eJxTclXSUUJD4YmZJZl56cpGpnAhx7z8PCBlaKZvYKlvZGBkCOKYWhkYo2mFqgMiC0NLI0yjIQhojKEBujEGegYGQMoEJGSIRR4ZGaBht8yi4hIFn8y8VLgSI0MCToUgXUNMMZipRCKow7G7TNcSCBDmoRtrgM4CB4EFgm9sAnWgZ4hCcGpRWWZyqoJvYl5iempual4JuQ7HsBaEGACQ+Fww">
+            <input type="TEXT" name="F2177" id="F2177" maxlength="25" value="" placeholder="Organisation Name">
+            <input type="TEXT" name="F38" id="reg_email" size="0035" maxlength="50" value="" placeholder="Work Email">
+            <select class="suptabde" name="F2178" id="F2178" size="1" onchange="selChg(2178);">
+            <option value="FOI%20and%20Case%20Management">Freedom of Information Management</option>
+            <option value="IT%20Service%20Management" selected>IT Service Management</option>
+            <option value="Customer%20Service%20Management">Customer Service & Support</option>
+            <option value="CAFM">Computer Aided Facilities Management</option>                            
+            </select>
+            <input class="Button" id="reg_submit" type="submit" value="Start free trial">
+          </form>
+        </div>
         <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/screenshots/dash-screenshot-2021.png" alt="itsm screenshot"></div>
-      </div>
-
-      <div class="form">
-        <form action="https://house-on-the-hill.com/mothership/supisacs.dll" method="POST" id="theForm" name="theForm" onsubmit="return Form_Validator(this);" enctype="multipart/form-data">
-          <input type="HIDDEN" id="ID" name="ID" value="52d8bfffda342ee">
-          <input type="HIDDEN" id="entrytoggle" name="entrytoggle" value="">
-          <input type="HIDDEN" id="selchange" name="selchange" value="">
-          <input type="HIDDEN" id="selparam1" name="selparam1" value="">
-          <input type="HIDDEN" id="AjaxUpdate" name="AjaxUpdate" value="">
-          <input type="HIDDEN" id="A" name="A" value="AnonCall">
-          <input type="HIDDEN" id="F1" name="F1" value="">
-          <input type="HIDDEN" id="F2158" name="F2158" value="0">
-          <input type="HIDDEN" id="NextStep" name="NextStep" value="">
-          <input type="HIDDEN" id="F2126" name="F2126" value="341">
-          <input type="HIDDEN" id="F2127_ctrl" name="F2127_ctrl" value="">
-          <input type="HIDDEN" id="F2128_ctrl" name="F2128_ctrl" value="">
-          <input type="HIDDEN" id="F2150_ctrl" name="F2150_ctrl" value="">
-          <input type="HIDDEN" id="F2129_ctrl" name="F2129_ctrl" value="">
-          <input type="HIDDEN" id="F2130_ctrl" name="F2130_ctrl" value="">
-          <input type="HIDDEN" id="F2148_ctrl" name="F2148_ctrl" value="">
-          <input type="HIDDEN" id="F2149_ctrl" name="F2149_ctrl" value="">
-          <input type="HIDDEN" id="ActResolved_ctrl" name="ActResolved_ctrl" value="">
-          <input type="HIDDEN" id="ActRespondedTo_ctrl" name="ActRespondedTo_ctrl" value="">
-          <input type="HIDDEN" id="F5006_ctrl" name="F5006_ctrl" value="">
-          <input type="HIDDEN" id="ContactId" name="ContactId" value="0">
-          <input type="HIDDEN" id="F" name="F" value="0">
-          <input type="HIDDEN" id="iViewHistory" name="iViewHistory" value="0">
-          <input type="HIDDEN" id="CCSM" name="CCSM" value="0">
-          <input type="HIDDEN" id="InitMedium" name="InitMedium" value="8192">
-          <input type="HIDDEN" id="SummViewFold" name="SummViewFold" value="0">
-          <input type="HIDDEN" id="CUOCHF" name="CUOCHF" value="1">
-          <input type="HIDDEN" id="savebtn" name="savebtn" value="">
-          <input type="HIDDEN" id="savebtn1" name="savebtn1" value="">
-          <input type="HIDDEN" id="CFID" name="CFID" value="eJxTclXSUUJD4YmZJZl56cpGpnAhx7z8PCBlaKZvYKlvZGBkCOKYWhkYo2mFqgMiC0NLI0yjIQhojKEBujEGegYGQMoEJGSIRR4ZGaBht8yi4hIFn8y8VLgSI0MCToUgXUNMMZipRCKow7G7TNcSCBDmoRtrgM4CB4EFgm9sAnWgZ4hCcGpRWWZyqoJvYl5iempual4JuQ7HsBaEGACQ+Fww">
-          <input type="TEXT" name="F2177" id="F2177" maxlength="25" value="" placeholder="Organisation Name">
-          <input type="TEXT" name="F38" id="reg_email" size="0035" maxlength="50" value="" placeholder="Work Email">
-          <select class="suptabde" name="F2178" id="F2178" size="1" onchange="selChg(2178);">
-          <option value="FOI%20and%20Case%20Management">Freedom of Information Management</option>
-          <option value="IT%20Service%20Management" selected>IT Service Management</option>
-          <option value="Customer%20Service%20Management">Customer Service & Support</option>
-          <option value="CAFM">Computer Aided Facilities Management</option>                            
-          </select>
-          <input class="Button" id="reg_submit" type="submit" value="Start free trial">
-        </form>
       </div>
     </div>
   </div>
