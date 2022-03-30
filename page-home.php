13167138f563135331b14d659af9f4328bc21baf
Luke Broadhead <lukebroadhead@houseonthehill.com>
2022-03-30 15:03:43 +0100

    home

--- page-home.php
+++ page-home.php
@@ -56,28 +56,27 @@
           <img height="25" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/50/bbraun-50.png" alt="bbraun Logo">
         </a>  
       </div>
-
-      <div class="quote">
-        <!--<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/screenshots/dash-screenshot-2021.png" alt="itsm screenshot">-->
-        <iframe width="760" height="515" src="https://www.youtube.com/embed/LUtaUuvzeZI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div>
     </div>
-    <div class="square"></div>
+  </div>
+
+  <div class="video">
+    <!--<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/screenshots/dash-screenshot-2021.png" alt="itsm screenshot">-->
+    <iframe width="760" height="515" src="https://www.youtube.com/embed/LUtaUuvzeZI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 
   <div class="solutions">
     <ul>
       <div class="text">
-        <h2 style="color: white;">What can I use House-on-the-Hill for?</h2>
-        <p style="color: white;">Out-of-the-Box House-on-the-Hill Software is ready for these solutions in the Cloud or On-Site. <br>Pick the solution which best reflects your organisation.</p>
+        <h2>What can I use House-on-the-Hill for?</h2>
+        <p>Out-of-the-Box House-on-the-Hill Software is ready for these solutions in the Cloud or On-Site. <br>Pick the solution which best reflects your organisation.</p>
         <a href="/price-plans"><p class="price"> <span>All HotH solutions start as low as £30 per agent, per month</span></p></a>
       </div>
 
       <li class="itsm">
-        <h3>IT Service <br>Management</h3>
         <a href="/it-service-management-software/">
           <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/itsm-header.jpg" alt="itsm Icon"></div>
         </a>
+        <h3>IT Service <br>Management</h3>
         <p>Deliver a modern ITIL driven service desk with a deep set of tools, automated workflows and a self-service portal. Where your users can raise tickets, access common service requests and search the knowledge base for FAQs.  </p>
         <div class="btn">
           <a class="sign-up" target="_blank" href="https://house-on-the-hill.com/mothership/register?A=QC&QC=QC000006">Try for free</a>
@@ -86,11 +85,11 @@
       </li>
 
       <li class="foi">
-        <h3>FOI, Case & Complaints <br>Management</h3>
         <a href="/foi-request-management/">
           <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/foi-header.jpg" alt="foi Icon"></div>
         </a>
-        <p>A popular fully paperless solution to aid your team with new requests logged by the public via the portal or by email. These cases can then be categorised into types and workflows such as FOI, Data Protection, Subject Access Requests, Complaints, Compliments, Information Governance and RoPA. </p>
+        <h3>FOI, Case & Complaints <br>Management</h3>
+        <p>Fully paperless solution to aid your team with new requests logged by the public via the portal or by email, these cases can then be categorised into types and workflows, such as FOI, Data Protection, Subject Access Requests, Complaints, Compliments, Information Governance and RoPA.</p>
         <div class="btn">
           <a class="sign-up" target="_blank" href="https://house-on-the-hill.com/mothership/register?A=QC&QC=QC000001">Try for free</a>
           <a class="more-info" href="/foi-request-management/">More HotH FOI ></a>
@@ -98,11 +97,11 @@
       </li>
 
       <li class="csm">
-        <h3>Customer Service <br>Management</h3>
         <a href="/customer-service-and-support/">
           <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/csm-header.jpg" alt="Customer Service Management Icon"></div>
         </a>
-        <p>Helps your sales team provide high quality customer support. Keep customers and staff happy by capturing key information through the branded portal, chat, email, feedback surveys and use data driven decisions from reports and KPIs to continually improve your service.</p>
+        <h3>Customer Service <br>Management</h3>
+        <p>Helps your sales team provide high quality customer support. Keep customers and staff happy by capturing key information through the branded portal, chat, email, feedback surveys, use data driven decisions from reports and KPIs to continually improve your service.</p>
         <div class="btn">
           <a class="sign-up" target="_blank" href="https://house-on-the-hill.com/mothership/register?A=QC&QC=QC000004">Try for free</a>
           <a class="more-info" href="/customer-service-and-support/">More HotH CSM ></a>
@@ -110,11 +109,11 @@
       </li>
 
       <li class="cafm">
-        <h3>Computer Aided Facilities <br>Management</h3>
         <a href="/facilities-management/">
           <div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/cafm-header.jpg" alt="CAFM Icon"></div>
         </a>
-        <p>Will allow you to manage your whole property portfolio, jobs and keep your engineers on the road. Your tool kit will include an engineer mobile app, reactive job logging, property maps, planned preventative maintenance, Job Sheets, Quotes, Invoices and more! </p>
+        <h3>Computer Aided Facilities <br>Management</h3>
+        <p>Will allow you to manage your whole property portfolio, jobs and keep your engineers on the road. Your tool kit will include an engineer mobile app, reactive job logging, property maps, planned preventative maintenance, Job Sheets, Quotes, Invoices and more.</p>
         <div class="btn">
           <a class="sign-up" target="_blank" href="https://house-on-the-hill.com/mothership/register?A=QC&QC=QC000003">Try for free</a>
           <a class="more-info" href="/facilities-management/">More HotH CAFM ></a>
