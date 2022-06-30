07350a7debb41c0dd6a80bfaeae256213aba94b5
Luke Broadhead <lukebroadhead@houseonthehill.com>
2022-06-30 15:41:11 +0100

    f

--- archive-features.php
+++ archive-features.php
@@ -126,7 +126,7 @@
 
     <div class="feature-list">
       <div class="sub-header">
-        <h2>Customisation</h2>
+        <h2>Customisation & Branding</h2>
         <p>
           Customise your service to yours and your customers needs.
         </p>
