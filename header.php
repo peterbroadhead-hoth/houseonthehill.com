a75015a782d9c6b4897d3cac2f8dc1182e5f4291
Luke Broadhead <lukebroadhead@houseonthehill.com>
2022-10-17 15:50:23 +0100

    home

--- header.php
+++ header.php
@@ -150,7 +150,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			<ul>
 				<li><a class="contact" href="/contact-us/" rel="noopener">Contact Us</a><img src="/wp-content/themes/houseonthehill.com/images/2023/underline.png"></li>
 				<li><a class="login" href="https://support.houseonthehill.com/" rel="noopener">Log in</a><img src="/wp-content/themes/houseonthehill.com/images/2023/underline.png"></li>
-				<li><a href="https://forms.office.com/pages/responsepage.aspx?id=OEYaXJY0K0eCJfAmS0vaMQGFda2P-j9Kj8fYAcaLAb5UMzVWU0NZOFlJODBHMldBSzZPSjYzR0I3NC4u" class="demo-btn">Book a demo</a><img src="/wp-content/themes/houseonthehill.com/images/2023/underline.png"></li>
+				<li><a target="_blank" href="https://forms.office.com/pages/responsepage.aspx?id=OEYaXJY0K0eCJfAmS0vaMQGFda2P-j9Kj8fYAcaLAb5UMzVWU0NZOFlJODBHMldBSzZPSjYzR0I3NC4u" class="demo-btn">Book a demo</a><img src="/wp-content/themes/houseonthehill.com/images/2023/underline.png"></li>
 				<li style="display: flex;"><a class="sign-up" href="https://house-on-the-hill.com/mothership/register" rel="noopener"target="_blank">Free trial</a></li>
 
 				<div class="reveal">
