821159d67834977aa7f35bddd4e7fa9eedb8323c
Luke Broadhead <lukebroadhead@houseonthehill.com>
2022-12-06 09:55:57 +0000

    gcloud

--- page-hothbot.php
+++ page-hothbot.php
@@ -2,6 +2,10 @@
 <div class="hothbot">
  <div class="header">
   <div class="container">
+   <div class="title">
+    <h1>Introducing HotH Bot</h1>
+    <p>Simplify the way you chat to your users with HotH Bot. <br>Our new feature built for easy communication to get the job done.</p>
+   </div>
   </div>
  </div>
 </div>
