c96e90a850d3e80ea89d239b68a2385c1375d9f4
Luke Broadhead <lukebroadhead@houseonthehill.com>
2022-12-07 11:01:02 +0000

    hothbot

--- page-hothbot.php
+++ page-hothbot.php
@@ -5,7 +5,49 @@
    <div class="title">
     <h1>Introducing HotH Bot</h1>
     <p>Simplify the way you chat to your users with HotH Bot. <br>Our new feature built for easy communication to get the job done.</p>
+    <a target="" href="/demo" class="sign-up">Book a demo</a>
    </div>
+  </div>
+ </div>
+
+ <ul>
+  <li>
+   <div class="img"><img src="https://houseonthehill.com/wp-content/uploads/2022/10/Faulty-Monitor-Ticket.png"></div>
+   <img class="purpblob1" src="https://houseonthehill.com/wp-content/themes/houseonthehill.com/images/2023/purpleblob.png">
+   <div class="text">
+    <img src="/wp-content/themes/houseonthehill.com/images/2023/150px/createpencil.png">
+    <h2>Create tickets within Teams</h2>
+    <p>Enter your ticket subject and click Create ticket to raise a ticket in your system, with the Subject pre-populated on the form.</p>
+   </div>
+  </li>
+
+  <li>
+   <div class="text">
+    <img src="/wp-content/themes/houseonthehill.com/images/2023/150px/replyicon.png">
+    <h2>Reply to your created tickets</h2>
+    <p>The ServiceDesk team will reply using the conversation tool within HotH. This reply will be presented within Teams, where you can write a reply and click OK to send.</p>
+   </div>
+   <div class="img"><img src="https://houseonthehill.com/wp-content/uploads/2022/10/ServiceDesk-Reply-HotHBot.png"></div>
+   <img class="purpblob2" src="https://houseonthehill.com/wp-content/themes/houseonthehill.com/images/2023/purpleblob2.png">
+  </li>
+
+  <li>
+   <div style="width: 100%; display: flex;" class="img"><img src="https://houseonthehill.com/wp-content/uploads/2022/10/ServiceDesk-Reply-HotH2.png" style="width: 70%; margin: auto;"></div>
+   <img class="purpblob3" src="https://houseonthehill.com/wp-content/themes/houseonthehill.com/images/2023/purpleblob3.png">
+   <div class="text" style="text-align: center; width: 70%">
+    <img src="">
+    <h2>Reply within HotH</h2>
+    <p>Widgets within the ticket allow end-users to reply to you and chat to get their issues sorted quickly.</p>
+   </div>
+  </li>
+ </ul>
+
+ <div class="demo">
+   <img class="whatsnew" src="/wp-content/themes/houseonthehill.com/images/2023/blackcircle.png">
+  <div class="text">
+   <h2>Watch HotH Bot in action</h2>
+   <p>Let one of our team show you how HotH Bot can benefit your business. We will create your own personalised trial for you to take away and use completely free for one month. <br>Fill in the form below and we will be in touch and give you a free guided tour.</p>
+   <a target="" href="/demo" class="sign-up">Book a demo</a>
   </div>
  </div>
 </div>
