6a24469b50e0bdda1c2e6329b22b1eaa8ba000c7
Luke Broadhead <lukebroadhead@houseonthehill.com>
2022-12-08 15:32:58 +0000

    social

--- page-social-values-statement.php
+++ page-social-values-statement.php
@@ -1,87 +1,75 @@
 <?php get_header();?>
-<div class="social-values-container">
-	<div class="social-values">
-		<div class="title">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/22icons/original/socialvalues.png" alt="social values graphic">
-			<h1>Our social values</h1>
-		  <p>At House-on-the-Hill, we are continually extending our efforts to support humanitarian causes, whether locally, nationally or internationally. As such, we are proud to support the following charities, fund-raising events and work experience programs.</p>
+<div class="social-values">
+	<div class="header">
+		<div class="container">
+			<div class="img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/2023/plant-pot.png"></div>
+			<div class="title">
+				<h1 style="">Our House, <br>our community, <br>this is what it <br>means to be HotH</h1>
+			</div>
+			<div class="text">
+				<!--<h4>What it means to be HotH</h4>-->
+				<p>At House-on-the-Hill, we are continually extending our efforts to support humanitarian causes, whether locally, 
+				nationally or internationally. As such, we are proud to support the following charities, fund-raising events and work experience programs.</p>
+			</div>
 		</div>
-	</div>	
-</div>
-<div class="social-values-content-container">
-	<div class="social-values-content">
-	  <ul>
-	  	<li>
-				<h2><span>National:</span></h2>
-				<h3>FareShare / Marcus Rashford</h3>
-				<p>Like many people across the UK we were touched by Marcus Rashford’s campaign with <a href="https://fareshare.org.uk/no-one-should-go-hungry-this-christmas/"> FareShare</a> to tackle childhood hunger across the UK. It has been an incredibly tough year for many with an ever-increasing number of people unable to afford the food they need to feed their families and themselves.</p>
-				<div class="img"><img src="/wp-content/uploads/2020/12/RASHFORD-FARESHARE.jpg"></div>
-			</li>
+	</div>
 
-			<li>
-				<h2><span>National:</span></h2>
-				<h3>Centrepoint – Give Homeless Young People a Future</h3>
+	<ul>
+		<li>
+			<div class="img" style="background: #D2FADF;"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/mcf.png"></div>
+			<div class="text">
+				<h3>Manchester Central Foodbank</h3>
+				<p>Providing three days’ nutritionally balanced emergency food and support to local people who are referred to us in crisis. We are part of a nationwide network of foodbanks, supported by The Trussell Trust, working to combat poverty and hunger across the UK. For more information, please see <a href="https://manchestercentral.foodbank.org.uk/">https://manchestercentral.foodbank.org.uk/</a></p>
+			</div>
+		</li>
+
+		<li>
+			<div class="text">
+				<h3>St Ann's Hospice</h3>
+				<p>Their purpose is to provide excellent care and support to people living with or affected by life-limiting illnesses. For more information, please see <a href="https://www.sah.org.uk/">https://www.sah.org.uk/</a></p>
+			</div>
+			<div class="img" style="background: #E8EFFF;"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/stanns.png"></div>
+		</li>
+
+		<li>
+			<div class="img" style="background: #8D6591;"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/akt.png"></div>
+			<div class="text">
+				<h3>Albert Kennedy Trust</h3>
+				<p>akt supports lgbtq+ young people aged 16-25 in the uk who are facing or experiencing homelessness or living in a hostile environment.<a href="https://www.akt.org.uk/what-we-do">https://www.akt.org.uk/what-we-do</a></p>
+			</div>
+		</li>
+
+		<li>
+			<div class="text">
+				<h3>Centrepoint</h3>
 				<p>Centrepoint works with young people who are homeless or at risk of homelessness in London, the North East and Yorkshire.  House-on-the-Hill has donated to Centrepoint since 2011, giving homeless young people a chance to turn their lives around, build firm foundations, and prepare for a future where they realise their potential. For more information, please see <a href="http://www.centrepoint.org.uk/">http://www.centrepoint.org.uk/</a></p>
-				<div class="img"><img src="/wp-content/uploads/2016/05/centrepoint-300x300.jpg"></div>
-			</li>
+			</div>
+			<div class="img" style="background: #FFE7D6;"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/centrepoint.png"></div>
+		</li>
 
-	  	<li>
-				<h2><span>Local:</span></h2>
-				<h3>Operation Oscar</h3>
-				<p>A very special cause we were able to support this year was <a href="https://just4children.org/children-helped2020/operationoscar/"> Operation Oscar!</a> Oscar is a young football fan that many of our employees have came to know at our local football team West Didsbury and Chorlton AFC. 6-year-old Oscar has cerebral Palsy and is raising funds for a life changing operation called SDR (Selective Dorsal Rhizotomy) which will potentially let him stand on his own two feet in future. This is a wonderful cause that you can learn more about <a href="https://just4children.org/children-helped2020/operationoscar/">here</a></p>
-				<div class="img"><img src="/wp-content/uploads/2020/12/OPERATION-OSCAR-TEAM-PIC.jpg"></div>
-			</li>
-
-	  	<li>
-				<h2><span>Worldwide:</span></h2>
+		<li>
+			<div class="img" style="background: #FFE8D2;"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/wv-logo.svg"></div>
+			<div class="text">
 				<h3>World Vision – Every Child Free from Fear</h3>
 				<p>World Vision is the world's largest international children’s charity improving the lives of the world’s poorest children by working closely with communities to tackle the root causes of poverty. For more than 20 years House-on-the-Hill has sponsored children around the world, through donating money and communicating with children personally, bringing real hope and changing their life, together with the lives of many more growing up alongside them. World Vision improves the lives of the world’s poorest children by working closely with communities to tackle the root causes of poverty. For more information, please see <a href="http://www.worldvision.org.uk/">http://www.worldvision.org.uk/</a></p>
-				<div class="img"><img src="/wp-content/uploads/2014/09/worldvision-300x300.jpeg"></div>
-			</li>
+			</div>
+		</li>
 
-			<li>
-				<h2><span>Local:</span></h2>
+		<li>
+			<div class="text">
 				<h3>Circus Starr – The Circus with a Purpose</h3>
 				<p>Circus Starr is a touring circus providing free seats for thousands of disadvantaged, disabled or vulnerable children, whilst helping to raise much needed funds for local charities. House-on-the-Hill’s donation provided opportunities for children in the Stockport area. For more information, please see <a href="http://www.circus-starr.org.uk/">http://www.circus-starr.org.uk/</a></p>
-				<div class="img"><img src="/wp-content/uploads/2016/05/Circus-Starr-300x200.jpg"></div>
-			</li>
+			</div>
+			<div class="img" style="background: #E59E92;"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/circus-starr.png"></div>
+		</li>
 
-			<li>
-				<h2><span>North-West:</span></h2>
+		<li>
+			<div class="img" style="background: #FFE7FF;"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/boothcentre.png"></div>
+			<div class="text">
 				<h3>Booth Centre – Activities, Advice and Support for Homeless People</h3>
 				<p>The Booth Centre, a local charity based in Manchester, offers support, advice and activities for those who are homeless or at risk of homelessness. House-on-Hill’s annual donations support the charity in giving people the chance to get involved with activities such as art, drama and sports which help them to build their skills and confidence. For more information, please see <a href="http://www.boothcentre.org.uk/">http://www.boothcentre.org.uk/</a></p>
-				<div class="img"><img src="/wp-content/uploads/2014/09/booth1-300x300.jpg"></div>
-			</li>
-
-			<li>
-				<h2><span>Local:</span></h2>
-				<h3>Mellor March – Helping Cancer Causes in our Community</h3>
-				<p>House-on-the-Hill has supported Mellor March, a walk around the boundary of the village of Mellor, since 2009 To date this local initiative has raised over £150,000 and every penny has been donated 'to cancer causes in our community'. For more information, please see <a href="http://www.mellor-march.org.uk/">http://www.mellor-march.org.uk/</a></p>
-			</li>
-
-			<li>
-				<h2><span>Local:</span></h2>
-				<h3>Local clubs/initiatives</h3>
-				<p>House-on-the-Hill has supported a number of local sporting clubs/initiatives including Rose Hill Rovers and Marple Skate Park providing opportunities for young people to be active and socialise in a safe environment with friends.</p>
-			</li>
-
-			<li>
-				<h2><span>Local:</span></h2>
-				<h3>Student Work Experience Programs</h3>
-				<p>For the past 10 years, House-on-the-Hill has provided Work Experience places to Year 10 students from Marple Hall School. Work Experience provides an introduction to the world of work. Students carry out a range of tasks and duties to learn valuable work skills such as punctuality, commitment and working with new and different people.</p>
-			</li>
-		</ul>
-	</div>
+			</div>
+		</li>
+	</ul>
 </div>
-
-
-<!--	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-	<div style="overflow:hidden;height:auto;width:100%;">
-		<div id="gmap_canvas" style="height:300px;width:100%;"></div>
-			<style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
-		</div>
-	<script type="text/javascript"> function init_map(){var myOptions = {zoom:10,center:new google.maps.LatLng(53.3952657,-2.068298499999969),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(53.3952657, -2.068298499999969)});infowindow = new google.maps.InfoWindow({content:"<b>House-on-the-Hill Software</b><br/>127 Stockport Road<br/>SK6 6AF Manchester" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
-	<div class="curtain"></div>
-				-->
-
 <?php get_footer();?>
