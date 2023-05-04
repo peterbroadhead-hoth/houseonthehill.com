47b869edaf7c16ac925ef491e9cf00b5c1adb594
lukebroadhead-hoth <83771381+lukebroadhead-hoth@users.noreply.github.com>
2023-05-04 11:45:52 +0100

    esm

--- page-enterprise-service-management.php
+++ page-enterprise-service-management.php
@@ -1,13 +1,90 @@
 <?php get_header();?>
-<div class="solution esm">
+<div class="esm">
   <div class="header">
     <div class="container">
       <div class="text">
         <h1>Making our house your organisation's digital home</h1>
-        <p>House-on-the-Hill helps you exceed your customer's expectations. 
-How? By becoming a strategic partner that strives to achieve a super-charged service that improves the way your team's work and delivers desired outcomes, fast.</p>
+        <p>House-on-the-Hill helps you exceed your customer's expectations. How? By becoming a strategic partner that strives to achieve a super-charged service that improves the way your team's work and delivers desired outcomes, fast.</p>
         <a target="" href="/demo" class="sign-up">Book a demo</a>
       </div>
     </div>
   </div>
+
+  <div class="outthebox">
+    <!--<div class="img">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/product/images/esmdash.png" alt="Enterprise service management dashboard"></a>
+    </div>-->
+    <div class="video-container">
+      <video width="100%" height="auto" autoplay loop muted>
+      <source src="<?php echo get_stylesheet_directory_uri(); ?>/images/product/videos/ESM-Dash.mp4" type="video/mp4">
+      </video>
+    </div>
+    <h2>Enterprise, straight Out-of-the-Box</h2>
+  </div>
+
+  <div class="lists">
+    <div class="list-one">
+      <div class="container">
+        <ul>
+          <li><h6>Multiple shared services housed within a single touchpoint</h6></li>
+          <li><h6>Fast, simple & recorded communication</h6></li>
+          <li><h6>Collaboration between departments</h6></li>
+          <li><h6>Customer Feedback Driven</h6></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="list-two">
+      <div class="container">
+        <ul>
+          <li><h6>Transparent reporting across all departments.</h6></li>
+          <li><h6>Sustainable low-code design to manage continual service improvements</h6></li>
+          <li><h6>Accessibility, a service desk for all.</h6></li>
+          <li><h6>Upgradeability. Enable the latest features whilst protecting customisations.</h6></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="objectives">
+    <div class="container">
+      <h2>Our ESM objectives</h2>
+      <ul>
+        <li>
+          <h3>Customer Value at the Core</h3>
+          <p>The main objective of House-on-the-Hill is to improve customer experience by providing a service that is easily accessible and delivers desired outcomes efficiently. Continual iterative feedback and reporting, keeps the service fresh and adaptable to changes in the service environment</p>
+        </li>
+        <li>
+          <h3>Control, Governance & Accountability</h3>
+          <p>Full organisational audit within the solution to track who did what and importantly, who didn’t do what! Reports pinpoint workload inefficiencies, bottlenecks and risks</p>
+        </li>
+        <li>
+          <h3>Improved transparency & Decision making</h3>
+          <p>Management can now attain a great understanding of what has been achieved and what hasn’t. Where is the value being generated or lost and how to communicate that with all stakeholders and make decisions to maximise or improve. Excecutive Portals Executive Departmental Reporting Digital Innovation and Productboards</p>
+        </li>
+        <li>
+          <h3>Orchestration of Services</h3>
+          <p>With HotH’s flexible process and workflow designer, new services can be released. retired and managed as per the customers' requirements. All the expertise is managed under one roof so service and technical knowledge is not siloed or potentially lost</p>
+        </li>
+        <li>
+          <h3>Consistent Quality and Standardisation</h3>
+          <p>A fit-for-purpose ESM solution ensures issues and requests do not slip through the net and are correctly and efficiently processed in accordance with agreed service levels. Bottlenecks and lost emails have become a thing of the past. ESM provides a platform for setting organisational standards, service models, look & feel and common ways of working</p>
+        </li>
+        <li>
+          <h3>Workload Reduction</h3>
+          <p>By promoting self-help, employees can find solutions via portals and bots. Reducing phone calls, emails, chats and walk-ups to the equivalent service desks. Freeing teams to focus on valuable tasks to continually improve their service delivery</p>
+        </li>
+        <li>
+          <h3>Reinvestment</h3>
+          <p>Cost and time saving benefits allow organisations to reinvest budgets, assets and teams effectively to promote a lean & sustainable future where resources are allocated accordingly</p>
+        </li>
+        <li>
+          <h3>Communication and Collaboration</h3>
+          <p>Work can be shared easily between Teams and departments, with all communications conducted centrally and saved for auditing and accountability</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
 <?php get_footer();?>
